a7cc08ada4dffa03bb039ce3383ad26c1706c8f8
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-07-07 01:09:53 +0300

    RapidApi IMDB Movies added.

---
+++ TraversalCoreProject/Areas/Admin/Controllers/ApiMovieController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TraversalCoreProject.Areas.Admin.Models;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiMovieController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiMovieViewModel> apiMovies = new List<ApiMovieViewModel>();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/"),
+                Headers =
+    {
+        { "x-rapidapi-key", "ec8301ee9emsh2badfd42cf7a397p11c50bjsn18b7907cc04e" },
+        { "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                apiMovies = JsonConvert.DeserializeObject<List<ApiMovieViewModel>>(body);
+                return View(apiMovies);
+            }
+        }
+    }
+}
