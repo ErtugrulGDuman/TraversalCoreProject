3e7c30673883ed043d1e79d255821ab2415c86c6
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-07-14 06:40:42 +0300

    ...

--- TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -29,7 +29,7 @@ namespace TraversalCoreProject.Controllers
             ViewBag.destID=id;
             var value = await _userManager.FindByNameAsync(User.Identity.Name);
             ViewBag.userId = value.Id;
-            var values = destinationManager.TGetDestinationWithGuides(id);
+            var values = destinationManager.TGetDestinationWithGuide(id);
             return View(values);
         }
         [HttpPost]
