7755aa52c98f1358edfd9f8e0d631786df9cf704
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-08-07 21:57:58 +0300

    Role assignments made.

--- TraversalCoreProject/Program.cs
+++ TraversalCoreProject/Program.cs
@@ -69,6 +69,11 @@ builder.Services.AddMvc(config =>
 
 builder.Services.AddMvc();
 
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Login/SignIn/";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -91,14 +96,6 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-      name: "areas",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
-});
 
 app.UseEndpoints(endpoints =>
 {
